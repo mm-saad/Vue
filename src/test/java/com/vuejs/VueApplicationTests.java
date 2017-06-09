32914c1494f2824eb7f7274d9a9bdd041a9b91a4
MM Saad <medmehdi.saad@hotmail.fr>
2017-06-09 13:30:14 +0200

    first commit

---
+++ src/test/java/com/vuejs/VueApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vuejs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class VueApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
